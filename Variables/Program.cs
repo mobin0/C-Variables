210d721828856845f1e7140425e5a8fd07faf1d3
Mobin Skaria <mobin0@berkeley.edu>
2022-05-31 07:59:28 -0700

    Variables TC Commit

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string houseName = "Purple";
+            int houseAge = 33;
+
+            Console.WriteLine($"My dog's name is {houseName}, He is {houseAge} years old");
+        }
+    }
+}
